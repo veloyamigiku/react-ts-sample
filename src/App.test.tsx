fd135e0a23c9a1f2c3cac5a7131f657b8e11e223
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-11-07 14:47:28 +0900

    ・ルーティングのテストを追加する。

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('ページ（ホーム）のルーティングのテスト', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('sample1')).toBeInTheDocument();
+  expect(screen.getByText('sample2')).toBeInTheDocument();
+});
+
+test('ページ（サンプル1）のルーティングのテスト', () => {
+  render(
+    <MemoryRouter initialEntries={['/sample1']}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('Home')).toBeInTheDocument();
+  expect(screen.getByText('sample1')).toBeInTheDocument();
+});
+
+test('ページ（サンプル2）のルーティングのテスト', () => {
+  render(
+    <MemoryRouter initialEntries={['/sample2']}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('Home')).toBeInTheDocument();
+  expect(screen.getByText('sample2')).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Sample1 from './components/Sample1';
 import Sample2 from './components/Sample2';
@@ -7,14 +7,12 @@ import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sample1" element={<Sample1 />} />
-        <Route path="/sample2" element={<Sample2 />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sample1" element={<Sample1 />} />
+      <Route path="/sample2" element={<Sample2 />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
